34801d45651ed7cec6f5939ea6a0359349e085e5
Danil0Ribeir0 <danilo.ribeiro18rs@gmail.com>
2025-12-09 21:19:23 -0300

    feat: adição de enums para gestos

--- src/utils/gesture_enums.py
+++ src/utils/gesture_enums.py
@@ -1,3 +1,9 @@
 from enum import Enum
 
 class HandGesture(Enum):
+    UNKNOWN = "Desconhecido"
+    OPEN_HAND = "Mão Aberta"
+    CLOSED_FIST = "Mão Fechada"
+    VICTORY = "Vitoria / Paz"
+    THUMBS_UP = "Joinha"
+    POINTER = "Apontando"
